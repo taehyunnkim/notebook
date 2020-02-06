e202900d282147ca2b8927b331d36e184500be19
Eric Kim <kim2000@uw.edu>
2020-02-06 10:10:45 -0800

    Updates

---
+++ precache-manifest.1b0e3d77f2c0334b280c5d9930455c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ed861943917ce3e8082a484dc142e9",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "289a5a9c861061a7ac24",
+    "url": "/notebook/static/css/main.99b37ad0.chunk.css"
+  },
+  {
+    "revision": "c842ca7714c7d41ba332",
+    "url": "/notebook/static/js/2.f54c2084.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/notebook/static/js/2.f54c2084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289a5a9c861061a7ac24",
+    "url": "/notebook/static/js/main.a5f9eda0.chunk.js"
+  },
+  {
+    "revision": "f4cb0bedee2faa4278ec",
+    "url": "/notebook/static/js/runtime-main.737af31e.js"
+  }
+]);
